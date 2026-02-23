1cbde771a1645d09d4d3192ddfffdc36f8696e90
GabrielCastillo2024 <gabrielenriquecc2024@gmail.com>
2026-02-23 12:18:48 +0100

    first: Programa java

---
+++ U3_ControlVersiones/src/GECC/Principal.java
@@ -0,0 +1,11 @@
+package GECC;
+
+public class Principal {
+	private String  nombre;
+	private String saludo="Holaaaaaaa bienvenido a mi programa ";
+	
+	public void saludar(String nombre,String saludo) {
+		System.out.println(saludo +" "+ nombre );
+	}
+	
+}
